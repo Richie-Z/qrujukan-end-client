9fe2a17d8010774ab010a8bfbecd546e92d17366
unknown <evolutionvalen@gmail.com>
2022-10-19 01:34:14 -0700

    latest

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,14 +1,17 @@
 <?php
 
-class Home extends CI_Controller{
+class Home extends CI_Controller
+{
 
-	public function __construct(){
-		
+	public function __construct()
+	{
+
 		parent::__construct();
 		$this->load->model('InputModel');
 	}
 
-	public function test(){
+	public function index()
+	{
 		$data['pasien'] = $this->InputModel->getPasien();
 		$data['rumah_sakit'] = $this->InputModel->getRumahSakit();
 
@@ -16,16 +19,10 @@ class Home extends CI_Controller{
 	}
 
 
-	public function getdokter(){
+	public function getdokter()
+	{
 		$rs_nama = $this->input->post('rs_nama', TRUE);
 		$data = $this->InputModel->getDokterRs($rs_nama);
 		echo json_encode($data);
 	}
-
-	
-
-
-
-	
-
 }
